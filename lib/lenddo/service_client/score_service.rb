3a048532f40c40302f6a577c223017365dbdbcc6
arjay salvadora <arjay@lenddo.com>
2017-09-28 18:18:39 +0800

    Use hash arguments Update unit test

--- lib/lenddo/service_client/score_service.rb
+++ lib/lenddo/service_client/score_service.rb
@@ -3,40 +3,40 @@ module Lenddo
     class ScoreService
       def application_score(application_id, partnerscript_id)
         response = signed_request(
-          "GET",
-          Lenddo.configuration.score_service,
-          "/ClientScore/#{application_id}",
-          { "partner_script_id" => partnerscript_id }
+          method: "GET",
+          host: Lenddo.configuration.score_service,
+          path: "/ClientScore/#{application_id}",
+          params: { "partner_script_id" => partnerscript_id }
         )
         JSON.parse(response.body)
       end
 
       def application_multiple_scores(application_id, partnerscript_id)
         response = signed_request(
-           "GET",
-           Lenddo.configuration.score_service,
-           "/ApplicationMultipleScores/#{application_id}",
-           { "partner_script_id" => partnerscript_id}
+          method: "GET",
+          host: Lenddo.configuration.score_service,
+          path: "/ApplicationMultipleScores/#{application_id}",
+          params: { "partner_script_id" => partnerscript_id }
         )
         JSON.parse(response.body)
       end
 
       def application_verification(application_id, partnerscript_id)
         response = signed_request(
-          "GET",
-          Lenddo.configuration.score_service,
-          "/ClientVerification/#{application_id}",
-          { "partner_script_id" => partnerscript_id }
+          method: "GET",
+          host: Lenddo.configuration.score_service,
+          path: "/ClientVerification/#{application_id}",
+          params: { "partner_script_id" => partnerscript_id }
         )
         JSON.parse(response.body)
       end
 
       def application_decision(application_id, partnerscript_id)
         response = signed_request(
-          "GET",
-          Lenddo.configuration.score_service,
-          "/ApplicationDecision/#{application_id}",
-          { "partner_script_id" => partnerscript_id}
+          method: "GET",
+          host: Lenddo.configuration.score_service,
+          path: "/ApplicationDecision/#{application_id}",
+          params: { "partner_script_id" => partnerscript_id }
         )
         JSON.parse(response.body)
       end
